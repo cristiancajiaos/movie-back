65c75ca43ab0b3f513e985b76087630ff82e2f4d
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-16 13:05:35 -0300

    Establecido prefix api, establecidas validaciones para el createMovieDto

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,1 +1,12 @@
-export class CreateMovieDto {}
+import { IsNumber, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreateMovieDto {
+
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  year: number; 
+}
